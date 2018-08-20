c064913e7a4a3dc97c0ca45c4cdf25ef9ac44666
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-08-20 18:06:33 -0500

    Update cash index example to read from xml

--- cash_index.py
+++ cash_index.py
@@ -1,17 +1,21 @@
 import csv
 from datetime import datetime
-from typing import NamedTuple
+from typing import NamedTuple, Iterator
+from datetime import date
 
 import numpy as np
 import pandas as pd
 
-from data.purchase_type import Arrangement, Seller
-from data.slaughter import SlaughterRecord, parse_line
+from data.hg201 import get_slaughter
+from data.model.purchase_type import Arrangement, Seller
+from data.model.slaughter import SlaughterRecord
 
 class CashTotal(NamedTuple):
   date: datetime
   total_weight: float
   total_value: float
+
+(DATE, TOTAL_WEIGHT, TOTAL_VALUE) = CashTotal._fields
 
 def filter_types(record: SlaughterRecord) -> bool:
   return record.seller == Seller.PRODUCER and \
@@ -20,25 +24,22 @@ def filter_types(record: SlaughterRecord) -> bool:
 def cash_total(record: SlaughterRecord) -> CashTotal:
   return CashTotal(record.date, record.total_weight, record.total_value)
 
-def get_cash_index():
-  with open('data/csv/LM_HG201-Barrows_Gilts.csv') as csvfile:
-    report = csv.reader(csvfile)
-
-    # skip header
-    next(report, None)
-
-    # parse csv into structured data and remove unneeded purchase types
-    records = filter(filter_types, map(parse_line, report))
-
-    # compute total weight and total value of each filtered record
-    cash_totals = map(cash_total, records)
-    data = pd.DataFrame.from_records(cash_totals, columns=CashTotal._fields)
+def compute_cash_index(records: Iterator[SlaughterRecord]) -> pd.DataFrame:
+  # filter all records by purchase type and compute total weight and total value of each
+  cash_totals = map(cash_total, filter(filter_types, records))
+  data = pd.DataFrame.from_records(cash_totals, columns=CashTotal._fields)
 
   # pivot by date and calculate total weight and value
-  table = pd.pivot_table(data, index=['date'], values=['total_weight', 'total_value'], aggfunc=np.sum)
+  table = pd.pivot_table(data, index=DATE, values=[TOTAL_WEIGHT, TOTAL_VALUE], aggfunc=np.sum)
 
   # compute daily cash index from two day sliding window of total value / total weight
   totals = table.rolling(2).sum().dropna()
-  totals['cash_index'] = np.divide(totals['total_value'], totals['total_weight'])
+  totals['cash_index'] = totals[TOTAL_VALUE] / totals[TOTAL_WEIGHT]
 
   return totals['cash_index']
+
+def get_cash_index(start_date: date, end_date: date = date.today()):
+  return compute_cash_index(get_slaughter(start_date, end_date))
+
+if __name__ == '__main__':
+  print(get_cash_index(date(2018, 8, 1)).tail())
