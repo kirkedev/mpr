3844d6d759eec49b3ef5b166db0bdc812bdeb394
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-08-20 19:26:28 -0500

    More refactoring

--- data/hg201.py
+++ data/hg201.py
@@ -1,10 +1,19 @@
+from enum import Enum
 from xml.etree.ElementTree import Element
 from typing import Iterator, Tuple, Optional as Maybe
 from datetime import date, datetime, timedelta
 
-from .api import get_optional, fetch, Report
+from .api import get_optional, fetch, filter_section, Report, Attributes
 from .model.slaughter import SlaughterRecord
 from .model.purchase_type import PurchaseType, Seller, Arrangement, Basis
+
+class Section(Enum):
+  SUMMARY = 'Summary'
+  BARROWS_AND_GILTS = 'Barrows/Gilts'
+  CARCASS_MEASUREMENTS = 'Carcass Measurements'
+  SOWS_AND_BOARS = 'Sows/Boars'
+  SCHEDULED_SWINE = '14-Day Scheduled Swine'
+  NEGOTIATED_BARROWS_AND_GILTS = 'Barrows/Gilts Negotiated'
 
 # Lookup table for purchase type descriptions as declared in the HG201 report from the USDA
 PURCHASE_TYPES = {
@@ -21,8 +30,8 @@ PURCHASE_TYPES = {
 Line = Tuple[str, str, str, Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str],
     Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str]]
 
-def parse(line: Line) -> SlaughterRecord:
-  """ Parses a line read from the daily Slaughtered Swine Report into a SlaughterRecord """
+def create_record(line: Line) -> SlaughterRecord:
+  """ Creates a SlaughterRecord from values retreived from the daily Slaughtered Swine Report """
   (_, date, purchase_type, head_count, base_price, net_price, low_price, high_price,
       live_weight, carcass_weight, *_, lean_percent) = line
 
@@ -42,60 +51,29 @@ def parse(line: Line) -> SlaughterRecord:
     carcass_weight = float(carcass_weight) if carcass_weight else None,
     lean_percent = float(lean_percent) if lean_percent else None)
 
-def parse_element(element: Element) -> Tuple[str, Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str],
-    Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str], Maybe[str]]:
+def get_attributes(attr: Attributes) -> Line:
+  return (
+    attr['report_date'],
+    attr['for_date_begin'],
+    attr['purchase_type'],
+    get_optional(attr, 'head_count'),
+    get_optional(attr, 'base_price'),
+    get_optional(attr, 'avg_net_price'),
+    get_optional(attr, 'lowest_net_price'),
+    get_optional(attr, 'highest_net_price'),
+    get_optional(attr, 'avg_live_weight'),
+    get_optional(attr, 'avg_carcass_weight'),
+    get_optional(attr, 'avg_sort_loss'),
+    get_optional(attr, 'avg_backfat'),
+    get_optional(attr, 'avg_loin_depth'),
+    get_optional(attr, 'loineye_area'),
+    get_optional(attr, 'avg_lean_percent'))
 
-  attr = element.attrib
-
-  purchase_type = attr['purchase_type']
-  head_count = get_optional(attr, 'head_count')
-  base_price = get_optional(attr, 'base_price')
-  net_price = get_optional(attr, 'avg_net_price')
-  low_price = get_optional(attr, 'lowest_net_price')
-  high_price = get_optional(attr, 'highest_net_price')
-  live_weight = get_optional(attr, 'avg_live_weight')
-  carcass_weight = get_optional(attr, 'avg_carcass_weight')
-  sort_loss = get_optional(attr, 'avg_sort_loss')
-  backfat = get_optional(attr, 'avg_backfat')
-  loin_depth = get_optional(attr, 'avg_loin_depth')
-  loineye_area = get_optional(attr, 'loineye_area')
-  lean_percent = get_optional(attr, 'avg_lean_percent')
-
-  return (purchase_type, head_count, base_price, net_price, low_price, high_price, live_weight,
-      carcass_weight, sort_loss, backfat, loin_depth, loineye_area, lean_percent)
-
-def generate_slaughter(reports: Iterator[Tuple[str, Element]]) -> Iterator[SlaughterRecord]:
-  depth = 0
-  report = None
-
-  for event, element in reports:
-    if element.tag == 'report':
-      report = element.attrib.get('label')
-
-    if element.tag == 'record':
-      if event == 'start':
-        if depth == 0:
-          report_date = element.attrib['report_date']
-          for_date = element.attrib['for_date_begin']
-
-        if depth == 1 and report == 'Barrows/Gilts':
-          yield parse((report_date, for_date, *parse_element(element)))
-
-        depth += 1
-
-      if event == 'end':
-        depth -= 1
-
-        if depth == 0:
-          report = None
+def create_records(attributes: Attributes) -> SlaughterRecord:
+  return create_record(get_attributes(attributes))
 
 def get_slaughter(start_date: date, end_date: date=date.today()) -> Iterator[SlaughterRecord]:
-  elements = fetch(Report.SLAUGHTERED_SWINE, start_date + timedelta(days=1), end_date)
+  response = fetch(Report.SLAUGHTERED_SWINE, start_date + timedelta(days=1), end_date)
+  attributes = filter_section(response, Section.BARROWS_AND_GILTS.value)
 
-  #skip top-level results metadata
-  next(elements, None)
-
-  #skip top-level report metadata
-  next(elements, None)
-
-  return generate_slaughter(elements)
+  return map(create_records, attributes)
