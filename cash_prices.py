d984f585e6f7323d8d0a4b554bd9767dec30f40d
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-08-29 21:41:05 -0500

    Fix cash prices and exploration notebook

--- cash_prices.py
+++ cash_prices.py
@@ -7,56 +7,57 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 
-from data.hg201 import get_slaughter
-from data.model.purchase_type import Arrangement, Seller
-from data.model.slaughter import SlaughterRecord
-
-from data.hg200 import get_purchases
-
-class PriceTotal(NamedTuple):
-  date: datetime
-  total_weight: float
-  total_value: float
-
-(DATE, TOTAL_WEIGHT, TOTAL_VALUE) = PriceTotal._fields
-
-(PRICE, PRICE_CHANGE, INDEX, INDEX_CHANGE) = ('price', 'price_change', 'index', 'index_change')
+from data.api.slaughter import get_slaughter, Record as SlaughterRecord
 
 def filter_types(record: SlaughterRecord) -> bool:
-  return record.seller == Seller.PRODUCER and \
-    record.arrangement in (Arrangement.NEGOTIATED, Arrangement.MARKET_FORMULA, Arrangement.NEGOTIATED_FORMULA)
+  return record.purchase_type in \
+    ('Prod. Sold Negotiated', 'Prod. Sold Swine or Pork Market Formula', 'Prod. Sold Negotiated Formula')
 
-def cash_total(record: SlaughterRecord) -> PriceTotal:
-  return PriceTotal(record.date, record.total_weight, record.total_value)
+def compute_weight(head_count: Series, carcass_weight: Series) -> Series:
+  return (head_count * carcass_weight).rename('total_weight')
+
+def compute_value(total_weight: Series, net_price: Series) -> Series:
+  return (total_weight * net_price).rename('total_value')
+
+def compute_price(total_value: Series, total_weight: Series) -> Series:
+  return (total_value / total_weight).rename('weighted_price')
 
 def compute_cash_index(records: Iterator[SlaughterRecord]) -> DataFrame:
-  # filter all records by purchase type and compute total weight and total value of each
-  cash_totals = map(cash_total, filter(filter_types, records))
-  data = DataFrame.from_records(cash_totals, columns=PriceTotal._fields)
+  data = DataFrame.from_records(records, columns=SlaughterRecord._fields)
 
-  # pivot by date and calculate total weight, total value, and weighted average price
-  table = pd.pivot_table(data, index=DATE, values=[TOTAL_WEIGHT, TOTAL_VALUE], aggfunc=np.sum)
-  table[PRICE] = table[TOTAL_VALUE] / table[TOTAL_WEIGHT]
-  table[PRICE_CHANGE] = table[PRICE] - table[PRICE].shift(1)
+  data['purchase_type'] = data['purchase_type'].replace({
+    'Prod. Sold Negotiated': 'Negotiated',
+    'Prod. Sold Swine or Pork Market Formula': 'Market Formula',
+    'Prod. Sold Negotiated Formula': 'Negotiated Formula'
+  })
 
-  # compute daily cash index from two day sliding window of total value / total weight
-  totals = table.rolling(2).sum().dropna()
-  table[INDEX] = totals[TOTAL_VALUE] / totals[TOTAL_WEIGHT]
-  table[INDEX_CHANGE] = table[INDEX] - table[INDEX].shift(1)
+  data = data.set_index(['date', 'purchase_type'])
 
-  return table.drop([TOTAL_VALUE, TOTAL_WEIGHT], axis=1)
+  total_weight = compute_weight(data['head_count'], data['carcass_weight'])
+  total_value = compute_value(total_weight, data['net_price'])
+  price = compute_price(total_value, total_weight)
 
-def get_cash_index(start_date: date, end_date: date = date.today()) -> DataFrame:
-  return compute_cash_index(get_slaughter(start_date, end_date))
+  totals = pd.pivot_table(pd.concat([total_weight, total_value], axis=1), index='date')
+  daily_price = compute_price(totals['total_value'], totals['total_weight'])
+  rolling_totals = totals.rolling(2).sum().dropna()
+  cash_index = compute_price(rolling_totals['total_value'], rolling_totals['total_weight']).rename('cme_index')
+
+  table = pd.concat([data['head_count'], data['carcass_weight'], price], axis=1).unstack()
+  cols = [(col, purchase_type) for (col, purchase_type) in table.columns if purchase_type != 'Negotiated Formula']
+  table = table[cols]
+
+  price_change = (daily_price - daily_price.shift(1)).rename('price_change')
+  index_change = (cash_index - cash_index.shift(1)).rename('index_change')
+
+  return pd.concat([table, daily_price, price_change, cash_index, index_change], axis=1)
+
+def get_cash_prices(days: int) -> DataFrame:
+  return compute_cash_index(filter(filter_types, get_slaughter(days + 2))).tail(days)
 
 if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser(description='Calculate the CME Lean Hog Index')
   parser.add_argument('--days', help='How many days to show', dest='days', type=int, default=10)
-
   days = parser.parse_args().days
-  today = date.today()
-  start_date = today - timedelta(days=days * 2)
 
-  pd.options.display.precision = 2
-  print(get_cash_index(start_date, today).tail(days))
+  print(get_cash_prices(days))
