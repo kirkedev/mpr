f6585410ffde71a4b8c2ecab8128281736f4935a
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-14 21:56:16 -0600

    Add steps to cleanup database before and after test script

--- mpr/data/db/__init__.py
+++ mpr/data/db/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 import tables
 
-path = Path('test/db' if os.environ['ENV'] == 'TEST' else 'mpr/data/db') / 'db.h5'
+path = Path(os.environ.get('DB') or 'mpr/data/db/db.h5')
 
 if path.is_file():
     connection = tables.open_file(str(path), 'a', driver='H5FD_CORE')
