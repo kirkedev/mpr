8a64c15a4fbfeecce8608b93f938d9b38d163b21
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-19 17:00:19 -0600

    Fix purchase parsing tests

--- test/api/test_purchase.py
+++ test/api/test_purchase.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 from datetime import date
+from numpy import isnan
+from numpy import isclose
+
+from mpr.data.api.purchase import parse_attributes
+
+from mpr.data.model.purchase_type import Seller
+from mpr.data.model.purchase_type import Arrangement
+from mpr.data.model.purchase_type import Basis
 
 from test.api import load_resource
-from mpr.data.api.purchase import parse_attributes
 
 records = list(load_resource('test/api/resources/purchase.xml'))
 assert len(records) == 7
@@ -15,37 +22,43 @@ class NegotiatedPurchaseTest(TestCase):
     def test_report_date(self):
         self.assertEqual(negotiated.date, date(2019, 1, 31))
 
-    def test_purchase_type(self):
-        self.assertEqual(negotiated.purchase_type, 'Negotiated (carcass basis)')
+    def test_seller(self):
+        self.assertEqual(negotiated.seller, Seller.ALL.to_ordinal())
+
+    def test_arrangement(self):
+        self.assertEqual(negotiated.arrangement, Arrangement.NEGOTIATED.to_ordinal())
+
+    def test_basis(self):
+        self.assertEqual(negotiated.basis, Basis.CARCASS.to_ordinal())
 
     def test_head_count(self):
         self.assertEqual(negotiated.head_count, 11325)
 
     def test_avg_price(self):
-        self.assertEqual(negotiated.avg_price, 50.7)
+        self.assertTrue(isclose(negotiated.avg_price, 50.7))
 
     def test_low_price(self):
-        self.assertEqual(negotiated.low_price, 48.0)
+        self.assertTrue(isclose(negotiated.low_price, 48.0))
 
     def test_high_price(self):
-        self.assertEqual(negotiated.high_price, 51.75)
+        self.assertTrue(isclose(negotiated.high_price, 51.75))
 
 
 class NegotiatedFormulaTest(TestCase):
     def test_report_date(self):
         self.assertEqual(negotiated_formula.date, date(2019, 1, 31))
 
-    def test_purchase_type(self):
-        self.assertEqual(negotiated_formula.purchase_type, 'Negotiated Formula (carcass basis)')
+    def test_arrangement(self):
+        self.assertEqual(negotiated_formula.arrangement, Arrangement.NEGOTIATED_FORMULA.to_ordinal())
 
     def test_head_count(self):
         self.assertEqual(negotiated_formula.head_count, 165)
 
     def test_avg_price(self):
-        self.assertEqual(negotiated_formula.avg_price, None)
+        self.assertTrue(isnan(negotiated_formula.avg_price))
 
     def test_low_price(self):
-        self.assertEqual(negotiated_formula.low_price, None)
+        self.assertTrue(isnan(negotiated_formula.low_price))
 
     def test_high_price(self):
-        self.assertEqual(negotiated_formula.high_price, None)
+        self.assertTrue(isnan(negotiated_formula.high_price))
