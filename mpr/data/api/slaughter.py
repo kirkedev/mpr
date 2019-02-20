cc27b2849f3006ab8fb12e35e6bf957b8ad961de
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-19 22:47:53 -0600

    More reorganization

--- mpr/data/api/slaughter.py
+++ mpr/data/api/slaughter.py
@@ -1,30 +1,15 @@
 from enum import Enum
-from typing import NamedTuple
 from typing import Iterator
 from datetime import date
-from datetime import datetime
 from datetime import timedelta
 from functools import singledispatch
 
-import numpy as np
-from numpy import datetime64
-from numpy import uint8
-from numpy import uint32
-from numpy import float32
-from numpy import recarray
-
-from mpr.data.model.purchase_type import purchase_types
-from mpr.data.model import Attributes
-from mpr.data.model import Date
-from mpr.data.model import opt_float
-from mpr.data.model import opt_int
+from mpr.data.model.slaughter import Record
 
 from . import Report
 from . import date_interval
 from . import fetch
 from . import filter_section
-
-date_format = "%m/%d/%Y"
 
 
 class Section(Enum):
@@ -36,61 +21,10 @@ class Section(Enum):
     # NEGOTIATED_BARROWS_AND_GILTS = 'Barrows/Gilts Negotiated'
 
 
-class Record(NamedTuple):
-    date: Date
-    seller: uint8
-    arrangement: uint8
-    basis: uint8
-    head_count: uint32
-    base_price: float32
-    net_price: float32
-    low_price: float32
-    high_price: float32
-    live_weight: float32
-    carcass_weight: float32
-    sort_loss: float32
-    backfat: float32
-    loin_depth: float32
-    loineye_area: float32
-    lean_percent: float32
-
-
-dtype = np.dtype(list(Record._field_types.items()))
-
-
-def to_array(records: Iterator[Record]) -> recarray:
-    return np.rec.array(list(records), dtype=dtype)
-
-
-def parse_attributes(attr: Attributes) -> Record:
-    report_date = datetime.strptime(attr['for_date_begin'], date_format).date()
-
-    purchase_type = attr['purchase_type']
-    (seller, arrangement, basis) = purchase_types[purchase_type]
-
-    return Record(
-        date=datetime64(report_date, 'D'),
-        seller=seller.to_ordinal(),
-        arrangement=arrangement.to_ordinal(),
-        basis=basis.to_ordinal(),
-        head_count=opt_int(attr, 'head_count') or 0,
-        base_price=opt_float(attr, 'base_price'),
-        net_price=opt_float(attr, 'avg_net_price'),
-        low_price=opt_float(attr, 'lowest_net_price'),
-        high_price=opt_float(attr, 'highest_net_price'),
-        live_weight=opt_float(attr, 'avg_live_weight'),
-        carcass_weight=opt_float(attr, 'avg_carcass_weight'),
-        sort_loss=opt_float(attr, 'avg_sort_loss'),
-        backfat=opt_float(attr, 'avg_backfat'),
-        loin_depth=opt_float(attr, 'avg_loin_depth'),
-        loineye_area=opt_float(attr, 'loineye_area'),
-        lean_percent=opt_float(attr, 'avg_lean_percent'))
-
-
 @singledispatch
 async def fetch_slaughter(start_date: date, end_date=date.today()) -> Iterator[Record]:
     response = await fetch(Report.SLAUGHTERED_SWINE, start_date + timedelta(days=1), end_date)
-    return map(parse_attributes, filter_section(response, Section.BARROWS_AND_GILTS.value))
+    return map(Record.from_attributes, filter_section(response, Section.BARROWS_AND_GILTS.value))
 
 
 @fetch_slaughter.register(int)
--- mpr/data/model/cutout.py
+++ mpr/data/model/cutout.py
@@ -1,12 +1,59 @@
 from abc import ABC
+from typing import NamedTuple
+from typing import Iterator
+from datetime import datetime
+
+import numpy as np
+from numpy import datetime64
+from numpy import float32
+from numpy import recarray
 
 from tables import Time32Col
 from tables import Float32Col
 from tables.tableextension import Row
 
-from mpr.data.api.cutout import Record
+from . import Attributes
+from . import Date
 
 from .observation import Observation
+
+date_format = "%m/%d/%Y"
+
+
+class Record(NamedTuple):
+    date: Date
+    primal_loads: float32
+    trimming_loads: float32
+    carcass_price: float32
+    loin_price: float32
+    butt_price: float32
+    picnic_price: float32
+    rib_price: float32
+    ham_price: float32
+    belly_price: float32
+
+    @classmethod
+    def from_attributes(cls, volume: Attributes, cutout: Attributes) -> 'Record':
+        report_date = datetime.strptime(volume['report_date'], date_format).date()
+
+        return cls(
+            date=datetime64(report_date, 'D'),
+            primal_loads=float32(volume['temp_cuts_total_load']),
+            trimming_loads=float32(volume['temp_process_total_load']),
+            carcass_price=float32(cutout['pork_carcass']),
+            loin_price=float32(cutout['pork_loin']),
+            butt_price=float32(cutout['pork_butt']),
+            picnic_price=float32(cutout['pork_picnic']),
+            rib_price=float32(cutout['pork_rib']),
+            ham_price=float32(cutout['pork_ham']),
+            belly_price=float32(cutout['pork_belly']))
+
+
+dtype = np.dtype(list(Record._field_types.items()))
+
+
+def to_array(records: Iterator[Record]) -> recarray:
+    return np.rec.array(list(records), dtype=dtype)
 
 
 class Cutout(Record, Observation, ABC):
