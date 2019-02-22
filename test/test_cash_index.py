685254908934688c46a8bb827519be76141dc826
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-21 18:33:04 -0600

    Refactor cash index tests

--- test/test_cash_index.py
+++ test/test_cash_index.py
@@ -24,27 +24,24 @@ prior_day = purchase_types.date == date(2019, 2, 18)
 class CashIndexTest(TestCase):
     # Cash prices for Feb 18-19, 2019
     # ftp://ftp.cmegroup.com/cash_settled_commodity_index_prices/daily_data/lean_hogs/LH190219.txt
+    @staticmethod
+    def weighted_avg_price(data: np.recarray) -> float:
+        weights = total_weight(data.head_count, data.carcass_weight)
+        values = total_value(weights, data.net_price)
+        prices = avg_price(np.nansum(values), np.nansum(weights))
+        return np.round(prices, decimals=2)
 
     def test_latest_weighted_price(self):
-        latest_data = purchase_types[latest]
-        weights = total_weight(latest_data.head_count, latest_data.carcass_weight)
-        values = total_value(weights, latest_data.net_price)
-        prices = avg_price(np.nansum(values), np.nansum(weights))
-        price = np.round(prices, decimals=2)
+        data = purchase_types[latest]
+        price = self.weighted_avg_price(data)
         self.assertTrue(np.isclose(price, 54.19))
 
     def test_prior_day_weighted_price(self):
-        prior_day_data = purchase_types[prior_day]
-        weights = total_weight(prior_day_data.head_count, prior_day_data.carcass_weight)
-        values = total_value(weights, prior_day_data.net_price)
-        prices = avg_price(np.nansum(values), np.nansum(weights))
-        price = np.round(prices, decimals=2)
+        data = purchase_types[prior_day]
+        price = self.weighted_avg_price(data)
         self.assertTrue(np.isclose(price, 54.08))
 
     def test_cme_lean_hog_index_price(self):
         data = purchase_types[latest | prior_day]
-        weights = total_weight(data.head_count, data.carcass_weight)
-        values = total_value(weights, data.net_price)
-        prices = avg_price(np.nansum(values), np.nansum(weights))
-        price = np.round(prices, decimals=2)
+        price = self.weighted_avg_price(data)
         self.assertTrue(np.isclose(price, 54.13))
