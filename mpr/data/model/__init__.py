db4af83443fa8a1daffe63157616db086c5595cb
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-23 17:17:12 -0600

    Move Attributes and Report

--- mpr/data/model/__init__.py
+++ mpr/data/model/__init__.py
@@ -1,10 +1,4 @@
-from typing import TypeVar
-from typing import Dict
-
 from numpy import dtype
-
-T = TypeVar('T')
-Attributes = Dict[str, str]
 
 date_type = dtype('datetime64[D]')
 Date = type(date_type)
