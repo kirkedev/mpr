b28f2ee2ee85e91f65567a076e278abe481362a3
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-23 17:30:06 -0600

    Add ability to dynamically import pytables objects

--- mpr/data/db/__init__.py
+++ mpr/data/db/__init__.py
@@ -1,12 +1,20 @@
+from os import environ
+from importlib import import_module
 from pathlib import Path
+from typing import Optional
 
-import os
 import tables
+from tables import Node
 
-path = Path(os.environ.get('DB', 'mpr/data/db/db.h5'))
+path = Path(environ.get('DB', 'mpr/data/db/db.h5'))
 
 if path.is_file():
     connection = tables.open_file(str(path), 'a', driver='H5FD_CORE')
 else:
     connection = tables.open_file(str(path), 'w', driver='H5FD_CORE')
     connection.create_group('/', 'mpr', 'USDA Mandatory Price Reporting')
+
+
+def get(group: str, table: Optional[str] = None) -> Node:
+    module = import_module(f".{group}", package='mpr.data.db')
+    return module.get(table)
