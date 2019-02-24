6c572742da38c0066ef281d592819bfe26352f48
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-23 18:53:52 -0600

    Remove rounding from tests

--- test/test_cash_prices.py
+++ test/test_cash_prices.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from numpy import allclose
-from numpy import round
 
 from mpr.data.api.slaughter import parse_attributes
 from mpr.cash_prices import cash_index
@@ -21,23 +20,19 @@ class TestCashPrices(TestCase):
     # Then I should see the Daily Weighted Average
     def test_daily_avg(self):
         daily_avg = report['Daily Avg Price']
-        self.assertTrue(allclose(round(daily_avg, decimals=2),
-            (56.40, 55.98, 55.78, 55.32, 55.16, 54.89, 54.64, 54.08, 54.19, 53.93)))
+        self.assertTrue(allclose(daily_avg, (56.40, 55.98, 55.78, 55.32, 55.16, 54.89, 54.64, 54.08, 54.19, 53.93)))
 
     # And I should see the change from the previous day
     def test_price_change(self):
         index_change = report['Price Change']
-        self.assertTrue(allclose(round(index_change, decimals=2),
-             (-0.25, -0.42, -0.20, -0.46, -0.16, -0.27, -0.25, -0.56, 0.11, -0.26)))
+        self.assertTrue(allclose(index_change, (-0.25, -0.42, -0.20, -0.46, -0.16, -0.27, -0.25, -0.56, 0.11, -0.26)))
 
     # And I should see the CME Index
     def test_cme_index(self):
         cme_index = report['CME Index']
-        self.assertTrue(allclose(round(cme_index, decimals=2),
-            (56.53, 56.14, 55.91, 55.55, 55.24, 55.02, 54.74, 54.43, 54.13, 54.06)))
+        self.assertTrue(allclose(cme_index, (56.53, 56.14, 55.91, 55.55, 55.24, 55.02, 54.74, 54.43, 54.13, 54.06)))
 
     # And I should see the change from the previous day
     def test_cme_change(self):
         index_change = report['Index Change']
-        self.assertTrue(allclose(round(index_change, decimals=2),
-            (-0.36, -0.39, -0.23, -0.36, -0.31, -0.22, -0.28, -0.31, -0.30, -0.07)))
+        self.assertTrue(allclose(index_change, (-0.36, -0.39, -0.23, -0.36, -0.31, -0.22, -0.28, -0.31, -0.30, -0.07)))
