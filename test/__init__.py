2a1ca6a6cd42aa534546b79f545b54ce9ce09aad
Andrew Kirkegaard <akirkegaard@pillartechnology.com>
2019-02-25 01:13:24 -0600

    Remove unused method to load xml from a string

--- test/__init__.py
+++ test/__init__.py
@@ -12,10 +12,6 @@ def parse_report(report: TextIO) -> Iterator[Attributes]:
     return parse_elements(elements)
 
 
-def load_report(report: str) -> Iterator[Attributes]:
-    return parse_report(StringIO(report))
-
-
 def load_resource(name: str) -> Iterator[Attributes]:
     with open(f"test/resources/{name}") as report:
         for element in parse_report(report):
