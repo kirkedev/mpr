0058faf15f75333b5ca11a6f554b96dd5e6c2aab
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-02-28 21:24:19 -0600

    Add cutout report

---
+++ mpr/reports/cutout/__main__.py
@@ -0,0 +1,10 @@
+import argparse
+import asyncio
+
+from . import get_cutout
+
+parser = argparse.ArgumentParser(description='Calculate the CME Cutout Index')
+parser.add_argument('--days', help='How many days to show', dest='days', type=int, default=10)
+
+days = parser.parse_args().days
+print(asyncio.run(get_cutout(days)))
