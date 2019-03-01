8648e065b2c6d6f8e671f2b06cff4e4b5348af97
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-02-28 20:36:02 -0600

    Fix notebook

--- mpr/reports/cash_index/__init__.py
+++ mpr/reports/cash_index/__init__.py
@@ -1,17 +1,20 @@
-from typing import Iterator
+from datetime import date
+from functools import singledispatch
+from pandas import DataFrame
 
-from mpr.data.model.slaughter import Slaughter
-from mpr.data.model.purchase_type import Arrangement
-
-
-total_weight = lambda head_count, weight: head_count * weight
-total_value = lambda weight, price: weight * price
-weighted_price = lambda value, weight: value / weight
+from mpr.data.api.slaughter import fetch_slaughter
+from ..calendar import recent_report_dates
+from .report import cash_index_report
 
 
-def filter_types(records: Iterator[Slaughter]) -> Iterator[Slaughter]:
-    return filter(lambda it: it.arrangement in (
-        Arrangement.NEGOTIATED,
-        Arrangement.MARKET_FORMULA,
-        Arrangement.NEGOTIATED_FORMULA
-    ), records)
+@singledispatch
+async def get_cash_prices(start: date, end=date.today()) -> DataFrame:
+    slaughter = await fetch_slaughter(start, end)
+    return cash_index_report(slaughter)
+
+
+@get_cash_prices.register(int)
+async def get_recent_cash_prices(n: int) -> DataFrame:
+    first, *_, last = recent_report_dates(n + 3)
+    cash_prices = await get_cash_prices(first, last)
+    return cash_prices.tail(n)
