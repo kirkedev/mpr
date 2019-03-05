2229dc4254e11caeece8b2b0838f5a25c9a2f1b9
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-03-05 10:49:33 -0600

    Add test_suite task to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages
 
 setup(
   name='mpr',
-  version='0.1',
+  version='0.1.1',
   author='Andrew Kirkegaard',
   author_email='andrew@kirke.dev',
   url='https://github.com/gumballhead/mpr',
   packages=find_packages(exclude=['test', 'test.*']),
   include_package_data=True,
   python_requires='>=3.7.*',
+  test_suite="test",
   install_requires=['aiohttp', 'numpy', 'pandas', 'statsmodels', 'tables'],
   extra_requires=['jupyter', 'seaborn'])
