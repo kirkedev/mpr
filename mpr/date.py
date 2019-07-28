a34d06f8b20e1a09399b20931cf5d4d5beae1aa9
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-07-27 19:00:23 -0500

    Add year to quarter

--- mpr/date.py
+++ mpr/date.py
@@ -9,26 +9,27 @@ from numpy import datetime64
 date64 = dtype('datetime64[D]')
 Date = type(date64)
 DateInterval = Tuple[date, date]
-
-
-def to_ordinal(dt: date64) -> uint32:
-    return dt.astype(date).toordinal()
-
-
-def from_ordinal(ordinal: uint32) -> date64:
-    return datetime64(date.fromordinal(ordinal), 'D')
+Quarter = Tuple[int, int]
 
 
 def from_string(date_string: str, date_format: str) -> date64:
     return datetime64(datetime.strptime(date_string, date_format).date(), 'D')
 
 
-def quarter(year: int, ordinal: int) -> DateInterval:
-    month = ordinal * 3 + 1
+def to_ordinal(it: date64) -> uint32:
+    return it.astype(date).toordinal()
+
+
+def from_ordinal(ordinal: uint32) -> date64:
+    return datetime64(date.fromordinal(ordinal), 'D')
+
+
+def to_quarter(it: date) -> Quarter:
+    return it.year, (it.month - 1) // 3
+
+
+def from_quarter(year: int, quarter: int) -> DateInterval:
+    month = quarter * 3 + 1
     start = date(year, month, 1)
-    end = date(year, month + 3, 1) if 0 <= ordinal < 3 else date(year + 1, 1, 1)
+    end = date(year, month + 3, 1) if 0 <= quarter < 3 else date(year + 1, 1, 1)
     return start, end
-
-
-def get_quarter(of: date) -> int:
-    return (of.month - 1) // 3
--- test/test_date.py
+++ test/test_date.py
@@ -1,30 +1,43 @@
 from datetime import date
 from unittest import TestCase
 
-from mpr.date import quarter, get_quarter
+from mpr.date import from_quarter
+from mpr.date import to_quarter
 
 
 class TestDate(TestCase):
     def test_first_quarter(self):
-        start, end = quarter(2019, 0)
+        start, end = from_quarter(2019, 0)
         self.assertEqual(start, date(2019, 1, 1))
         self.assertEqual(end, date(2019, 4, 1))
-        self.assertEqual(get_quarter(date(2019, 2, 15)), 0)
+
+        year, quarter = to_quarter(date(2019, 2, 15))
+        self.assertEqual(year, 2019)
+        self.assertEqual(quarter, 0)
 
     def test_second_quarter(self):
-        start, end = quarter(2019, 1)
+        start, end = from_quarter(2019, 1)
         self.assertEqual(start, date(2019, 4, 1))
         self.assertEqual(end, date(2019, 7, 1))
-        self.assertEqual(get_quarter(date(2019, 5, 15)), 1)
+
+        year, quarter = to_quarter(date(2019, 5, 15))
+        self.assertEqual(year, 2019)
+        self.assertEqual(quarter, 1)
 
     def test_third_quarter(self):
-        start, end = quarter(2019, 2)
+        start, end = from_quarter(2019, 2)
         self.assertEqual(start, date(2019, 7, 1))
         self.assertEqual(end, date(2019, 10, 1))
-        self.assertEqual(get_quarter(date(2019, 8, 15)), 2)
+
+        year, quarter = to_quarter(date(2019, 8, 15))
+        self.assertEqual(year, 2019)
+        self.assertEqual(quarter, 2)
 
     def test_fourth_quarter(self):
-        start, end = quarter(2019, 3)
+        start, end = from_quarter(2019, 3)
         self.assertEqual(start, date(2019, 10, 1))
         self.assertEqual(end, date(2020, 1, 1))
-        self.assertEqual(get_quarter(date(2019, 11, 15)), 3)
+
+        year, quarter = to_quarter(date(2019, 11, 15))
+        self.assertEqual(year, 2019)
+        self.assertEqual(quarter, 3)
