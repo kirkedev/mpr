791fed79eee7b88c8291e7ccc7b06b67f6c9960a
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2019-10-09 16:41:55 -0500

    Minor fixes

--- features/purchases.feature
+++ features/purchases.feature
@@ -1,5 +1,5 @@
 Feature: Direct Hog Purchases
-  Scenario: Daily Direct Hog Afternoon
+  Scenario: Daily Direct Hog Prior Day
     Given a date range from June 1st to June 30th, 2019
     When I request the daily direct hog prior day report
     Then I will receive a list of daily hog purchase records for the prior day
--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
   name='mpr',
   version='0.1.1',
   author='Andrew Kirkegaard',
-  author_email='andrew@kirke.dev',
+  author_email='andrew.kirkegaard@gmail.com',
   url='https://github.com/gumballhead/mpr',
   packages=find_packages(exclude=['test', 'test.*']),
+  keywords=['usda', 'agriculture', 'livestock', 'commodities', 'trading'],
   include_package_data=True,
   python_requires='>=3.7.*',
   test_suite="test",
-  install_requires=['aiohttp', 'numpy', 'pandas', 'statsmodels', 'tables', 'behave'],
-  extra_requires=['jupyter', 'seaborn'])
+  install_requires=['aiohttp', 'numpy', 'pandas', 'statsmodels', 'tables'])
