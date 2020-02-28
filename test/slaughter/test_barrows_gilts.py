9dcd2ef95c5e317a084059a0811629436e22f05b
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-02-27 22:40:13 -0600

    Add tests for parsing all barrows and gilts slaughter purchase types

---
+++ test/slaughter/test_barrows_gilts.py
@@ -0,0 +1,168 @@
+from datetime import date
+from numpy import isnan
+from numpy import isclose
+
+from mpr.slaughter.api import parse_attributes
+from mpr.slaughter.model import to_array
+from mpr.purchase_type import Seller, Arrangement, Basis
+
+from test import load_resource
+
+attributes = list(load_resource('api/slaughter.xml'))
+assert len(attributes) == 8
+
+negotiated = parse_attributes(attributes[0])
+other_market_formula = parse_attributes(attributes[1])
+swine_pork_market_formula = parse_attributes(attributes[2])
+other_purchase_agreement = parse_attributes(attributes[3])
+negotiated_formula = parse_attributes(attributes[4])
+packer_owned = parse_attributes(attributes[7])
+
+
+def test_negotiated():
+    assert negotiated.date == date(2019, 2, 1)
+    assert negotiated.report_date == date(2019, 2, 4)
+    assert negotiated.seller == Seller.PRODUCER
+    assert negotiated.arrangement == Arrangement.NEGOTIATED
+    assert negotiated.basis == Basis.ALL
+    assert isclose(negotiated.head_count, 12771)
+    assert isclose(negotiated.base_price, 51.8)
+    assert isclose(negotiated.net_price, 53.26)
+    assert isclose(negotiated.low_price, 43.57)
+    assert isclose(negotiated.high_price, 57.85)
+    assert isclose(negotiated.live_weight, 273.54)
+    assert isclose(negotiated.carcass_weight, 205.41)
+    assert isclose(negotiated.sort_loss, -2.16)
+    assert isclose(negotiated.backfat, 0.61)
+    assert isclose(negotiated.loin_depth, 2.61)
+    assert isclose(negotiated.loineye_area, 7.83)
+    assert isclose(negotiated.lean_percent, 55.60)
+    assert isclose(negotiated.total_weight, 2_623_291.16)
+    assert isclose(negotiated.total_value, 13_9716_484.52)
+    assert isclose(negotiated.avg_price, 53.26)
+
+
+def test_other_market_formula():
+    assert other_market_formula.date == date(2019, 2, 1)
+    assert other_market_formula.report_date == date(2019, 2, 4)
+    assert other_market_formula.seller == Seller.PRODUCER
+    assert other_market_formula.arrangement == Arrangement.OTHER_MARKET_FORMULA
+    assert other_market_formula.basis == Basis.ALL
+    assert other_market_formula.head_count == 71_962
+    assert isclose(other_market_formula.base_price, 56.81)
+    assert isclose(other_market_formula.net_price, 59.14)
+    assert isclose(other_market_formula.low_price, 47.51)
+    assert isclose(other_market_formula.high_price, 69.91)
+    assert isclose(other_market_formula.live_weight, 288.18)
+    assert isclose(other_market_formula.carcass_weight, 218.50)
+    assert isclose(other_market_formula.sort_loss, -2.14)
+    assert isclose(other_market_formula.backfat, 0.61)
+    assert isclose(other_market_formula.loin_depth, 2.73)
+    assert isclose(other_market_formula.loineye_area, 8.23)
+    assert isclose(other_market_formula.lean_percent, 56.29)
+    assert isclose(other_market_formula.total_weight, 15_723_697)
+    assert isclose(other_market_formula.total_value, 929_899_440.58)
+    assert isclose(other_market_formula.avg_price, 59.14)
+
+
+def test_swine_pork_market_formula():
+    assert swine_pork_market_formula.date == date(2019, 2, 1)
+    assert swine_pork_market_formula.report_date == date(2019, 2, 4)
+    assert swine_pork_market_formula.seller == Seller.PRODUCER
+    assert swine_pork_market_formula.arrangement == Arrangement.MARKET_FORMULA
+    assert swine_pork_market_formula.basis == Basis.ALL
+    assert swine_pork_market_formula.head_count == 256_439
+    assert isclose(swine_pork_market_formula.base_price, 55.53)
+    assert isclose(swine_pork_market_formula.net_price, 57.65)
+    assert isclose(swine_pork_market_formula.low_price, 37.89)
+    assert isclose(swine_pork_market_formula.high_price, 67.65)
+    assert isclose(swine_pork_market_formula.live_weight, 286.91)
+    assert isclose(swine_pork_market_formula.carcass_weight, 216.06)
+    assert isclose(swine_pork_market_formula.sort_loss, -1.85)
+    assert isclose(swine_pork_market_formula.backfat, 0.64)
+    assert isclose(swine_pork_market_formula.loin_depth, 2.65)
+    assert isclose(swine_pork_market_formula.loineye_area, 7.97)
+    assert isclose(swine_pork_market_formula.lean_percent, 56.00)
+    assert isclose(swine_pork_market_formula.total_weight, 55_406_210.34)
+    assert isclose(swine_pork_market_formula.total_value, 3_194_168_026.10)
+    assert isclose(swine_pork_market_formula.avg_price, 57.65)
+
+
+def test_other_purchase_agreement():
+    assert other_purchase_agreement.date == date(2019, 2, 1)
+    assert other_purchase_agreement.report_date == date(2019, 2, 4)
+    assert other_purchase_agreement.seller == Seller.PRODUCER
+    assert other_purchase_agreement.arrangement == Arrangement.OTHER_PURCHASE
+    assert other_purchase_agreement.basis == Basis.ALL
+    assert other_purchase_agreement.head_count == 134_335
+    assert isclose(other_purchase_agreement.base_price, 63.31)
+    assert isclose(other_purchase_agreement.net_price, 64.15)
+    assert isclose(other_purchase_agreement.low_price, 47.39)
+    assert isclose(other_purchase_agreement.high_price, 77.44)
+    assert isclose(other_purchase_agreement.live_weight, 284.97)
+    assert isclose(other_purchase_agreement.carcass_weight, 213.73)
+    assert isclose(other_purchase_agreement.sort_loss, -1.51)
+    assert isclose(other_purchase_agreement.backfat, .71)
+    assert isclose(other_purchase_agreement.loin_depth, 2.49)
+    assert isclose(other_purchase_agreement.loineye_area, 7.48)
+    assert isclose(other_purchase_agreement.lean_percent, 55.18)
+    assert isclose(other_purchase_agreement.total_weight, 28_711_419.55)
+    assert isclose(other_purchase_agreement.total_value, 1_841_837_564.13)
+    assert isclose(other_purchase_agreement.avg_price, 64.15)
+
+
+def test_negotiated_formula():
+    assert negotiated_formula.date == date(2019, 2, 1)
+    assert negotiated_formula.report_date == date(2019, 2, 4)
+    assert negotiated_formula.arrangement == Arrangement.NEGOTIATED_FORMULA
+    assert negotiated_formula.basis == Basis.ALL
+    assert negotiated_formula.head_count == 683
+    assert isnan(negotiated_formula.base_price)
+    assert isnan(negotiated_formula.net_price)
+    assert isnan(negotiated_formula.low_price)
+    assert isnan(negotiated_formula.high_price)
+    assert isnan(negotiated_formula.live_weight)
+    assert isnan(negotiated_formula.carcass_weight)
+    assert isnan(negotiated_formula.sort_loss)
+    assert isnan(negotiated_formula.backfat)
+    assert isnan(negotiated_formula.loin_depth)
+    assert isnan(negotiated_formula.loineye_area)
+    assert isnan(negotiated_formula.lean_percent)
+    assert isnan(negotiated_formula.total_weight)
+    assert isnan(negotiated_formula.total_value)
+    assert isnan(negotiated_formula.avg_price)
+
+
+def test_packer_owned():
+    assert packer_owned.date == date(2019, 2, 1)
+    assert packer_owned.report_date == date(2019, 2, 4)
+    assert packer_owned.arrangement == Arrangement.PACKER_OWNED
+    assert packer_owned.basis == Basis.ALL
+    assert packer_owned.head_count == 252_211
+    assert isnan(packer_owned.base_price)
+    assert isnan(packer_owned.net_price)
+    assert isnan(packer_owned.low_price)
+    assert isnan(packer_owned.high_price)
+    assert isclose(packer_owned.live_weight, 287.07)
+    assert isclose(packer_owned.carcass_weight, 218.47)
+    assert isnan(packer_owned.sort_loss)
+    assert isclose(packer_owned.backfat, .67)
+    assert isclose(packer_owned.loin_depth, 2.52)
+    assert isclose(packer_owned.loineye_area, 7.57)
+    assert isclose(packer_owned.lean_percent, 54.11)
+    assert isclose(packer_owned.total_weight, 55_100_537.17)
+    assert isnan(packer_owned.total_value)
+    assert isnan(packer_owned.avg_price)
+
+
+def test_record_array():
+    records = to_array([
+        negotiated,
+        other_market_formula,
+        swine_pork_market_formula,
+        negotiated_formula,
+        packer_owned
+    ])
+
+    assert len(records) == 5
+    assert all(records.date == date(2019, 2, 1))
