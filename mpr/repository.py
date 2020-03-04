187f57b3326a5bcec8202c72050bec57b3cd83df
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-04 16:57:23 -0600

    Extract archive slicing

--- mpr/repository.py
+++ mpr/repository.py
@@ -7,6 +7,7 @@ from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
+from typing import Union
 from zipfile import ZipFile
 from zipfile import ZIP_DEFLATED
 
@@ -21,6 +22,19 @@ Data = Dict[Section, List[Attributes]]
 Result = Tuple[Week, Optional[Data]]
 
 
+def get_section(archive: ZipFile, section: Section) -> List[Attributes]:
+    return json.loads(archive.read(f"{section}.json"))
+
+
+def get_sections(archive: ZipFile, *sections: Section) -> Tuple[List[Attributes], ...]:
+    return tuple(json.loads(archive.read(f"{section}.json")) for section in sections)
+
+
+def get_report(archive: ZipFile) -> Data:
+    sections = (Path(name).stem for name in archive.namelist())
+    return {section: json.loads(archive.read(f"{section}.json")) for section in sections}
+
+
 class Archive(PathLike):
     root: Path
     week: Week
@@ -32,17 +46,16 @@ class Archive(PathLike):
     def __fspath__(self) -> str:
         return str(self.root / str(self.week))
 
-    def get(self, *sections: Section) -> Data:
+    def get(self, *sections: Section) -> Union[Data, List[Attributes], Tuple[List[Attributes]]]:
         n = len(sections)
 
         with ZipFile(self) as archive:
             if n == 0:
-                sections = (Path(name).stem for name in archive.namelist())
-
-            if n == 1:
-                return json.loads(archive.read(f"{sections[0]}.json"))
-
-            return {section: json.loads(archive.read(f"{section}.json")) for section in sections}
+                return get_report(archive)
+            elif n == 1:
+                return get_section(archive, sections[0])
+            else:
+                return get_sections(archive, *sections)
 
     def save(self, data: Data):
         with ZipFile(self, 'w', ZIP_DEFLATED) as archive:
@@ -54,7 +67,7 @@ class Repository(PathLike):
     root: Path
     report: Report
 
-    def __init__(self, root: Path, report: Report):
+    def __init__(self, report: Report, root=Path("data")):
         self.root = root
         self.report = report
         path = Path(self)
