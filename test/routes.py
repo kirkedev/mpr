e43d42b109aa25fecb1c2536991110605073311e
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-05 15:14:54 -0600

    Add prior day purchase reports to test server

--- test/routes.py
+++ test/routes.py
@@ -5,20 +5,22 @@ from aiohttp.web import FileResponse
 from aiohttp.web import get
 
 from mpr.report import Report
+from mpr.report import lm_hg200
 from mpr.report import lm_hg201
 from mpr.report import lm_pk602
 
 
 def route(report: Report):
     async def handler(request: Request) -> FileResponse:
-        filters = json.loads(request.query.get('filter')).get('filters')
-        start, end = filters[0].get('values')
+        filters = json.loads(request.query.get('filter'))
+        start, end = filters.get('filters')[0].get('values')
         return FileResponse(f"test/resources/server/{report}_{start}_{end}.xml")
 
     return get(f"/ws/report/v1/hogs/{report}", handler)
 
 
 routes = [
+    route(lm_hg200),
     route(lm_hg201),
     route(lm_pk602)
 ]
