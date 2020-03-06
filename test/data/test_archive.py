fa0752ea2d22eff9c92393f793e03ed67cdc8ade
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-05 20:02:36 -0600

    Implement incremental updates to archvies

--- test/data/test_archive.py
+++ test/data/test_archive.py
@@ -10,27 +10,31 @@ from mpr.report import CutoutReport
 
 @fixture
 async def archive(tmp_path: Path):
-    archive = Archive(tmp_path, Week.withdate(date(2018, 8, 20)))
+    report_date = date(2018, 8, 20)
 
-    archive.save({
-        CutoutReport.Section.CUTOUT: [{
-            'report_date': '08/20/2018',
-            'pork_carcass': '67.18',
-            'pork_loin': '75.51',
-            'pork_butt': '89.55',
-            'pork_picnic': '41.82',
-            'pork_rib': '113.95',
-            'pork_ham': '57.52',
-            'pork_belly': '77.77'
-        }],
-        CutoutReport.Section.VOLUME: [{
-            'report_date': '08/20/2018',
-            'temp_cuts_total_load': '334.74',
-            'temp_process_total_load': '39.61'
-        }]
-    })
+    return Archive.create(tmp_path, Week.withdate(report_date), report_date,
+    [{
+        'slug': 'LM_PK602',
+        'label': 'Cutout and Primal Values',
+        'report_date': '08/20/2018',
+        'pork_carcass': '67.18',
+        'pork_loin': '75.51',
+        'pork_butt': '89.55',
+        'pork_picnic': '41.82',
+        'pork_rib': '113.95',
+        'pork_ham': '57.52',
+        'pork_belly': '77.77'
+    }, {
+        'slug': 'LM_PK602',
+        'label': 'Current Volume',
+        'report_date': '08/20/2018',
+        'temp_cuts_total_load': '334.74',
+        'temp_process_total_load': '39.61'
+    }])
 
-    return archive
+
+def test_path(archive: Archive):
+    assert Path(archive).name == "2018W34D00.zip"
 
 
 def test_get_report_section(archive: Archive):
@@ -52,3 +56,24 @@ def test_get_full_report(archive: Archive):
 
     volume = report[CutoutReport.Section.VOLUME]
     assert len(volume) == 1
+
+
+def test_update_archive(archive: Archive):
+    archive.update(date(2018, 8, 21), [{
+        'slug': 'LM_PK602',
+        'label': 'Cutout and Primal Values',
+        'report_date': '08/21/2018',
+        'pork_carcass': '66.19',
+        'pork_loin': '74.05',
+        'pork_butt': '90.15',
+        'pork_picnic': '40.61',
+        'pork_rib': '115.35',
+        'pork_ham': '55.92',
+        'pork_belly': '76.45'
+    }, {
+        'slug': 'LM_PK602',
+        'label': 'Current Volume',
+        'report_date': '08/21/2018',
+        'temp_cuts_total_load': '396.30',
+        'temp_process_total_load': '52.57'
+    }])
