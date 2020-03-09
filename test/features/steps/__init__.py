9cf4732881bcb74dc40344b570d47ce4fc9a28d0
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-08 19:47:08 -0500

    Add return signature to server definition

--- test/features/steps/__init__.py
+++ test/features/steps/__init__.py
@@ -14,13 +14,13 @@ def format_number(number: int) -> str:
     return '{0:.{1}f}'.format(number, 0)
 
 
+def server() -> TestServer:
+    app = Application()
+    app.add_routes(routes)
+    return TestServer(app, port=8080)
+
+
 @given('a date range from June 1st to June 30th, 2019')
 def set_date_range(context):
     context.start = date(2019, 6, 1)
     context.end = date(2019, 6, 30)
-
-
-def server():
-    app = Application()
-    app.add_routes(routes)
-    return TestServer(app, port=8080)
