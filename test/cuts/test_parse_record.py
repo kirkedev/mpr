937179eb4664a696d9c5bf12ae298c723ca8a854
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-11 17:25:05 -0500

    Rename some tests

--- test/cuts/test_parse_record.py
+++ test/cuts/test_parse_record.py
@@ -56,11 +56,3 @@ def test_fourth():
     assert isclose(record.avg_price, 142.71)
     assert isclose(record.low_price, 140.50)
     assert isclose(record.high_price, 152.00)
-
-
-# @mark.asyncio
-# async def test_query():
-#     async with server():
-#         records = await fetch_cuts(lm_pk602, CutType.BELLY, date(2019, 6, 1), date(2019, 6, 10))
-#
-#     assert len(list(records)) == 24
