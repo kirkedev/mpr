9e125380dcce94fe06551a83cd8c6f14e96b202e
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-13 16:02:07 -0500

    Remove print statement

--- mpr/purchase/__init__.py
+++ mpr/purchase/__init__.py
@@ -9,6 +9,5 @@ from ..data.repository import Repository
 
 async def daily(start: date, end: date) -> Iterator[Purchase]:
     end = min(lm_hg200.latest, end)
-    print(lm_hg200.latest)
     purchases = await Repository(lm_hg200).query(start, end, lm_hg200.Section.BARROWS_AND_GILTS)
     return map(parse_record, purchases)
