58349cdafbd88c51d436f8497f101e8c7dffd2f9
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-03-17 16:30:05 -0500

    Remove querying repository for full reports

--- test/data/test_repository.py
+++ test/data/test_repository.py
@@ -7,7 +7,6 @@ from pytest import mark
 from mpr.cutout.report import CutoutReport
 from mpr.cutout.report import lm_pk602
 from mpr.data.repository import Repository
-from mpr.data.repository import merge_reports
 from mpr.data.repository import slice_dates
 from test.server import server
 
@@ -30,26 +29,6 @@ def test_slice_dates():
     }]
 
     assert len(list(slice_dates(items, date(2019, 6, 1), date(2019, 6, 6)))) == 4
-
-
-def test_merge_reports():
-    records = [{
-        'one': [{'report_date': '05/29/2019'}, {'report_date': '05/20/2019'}, {'report_date': '05/31/2019'}],
-        'two': [{'report_date': '06/03/2019'}]
-    }, {
-        'one': [{'report_date': '06/05/2019'}],
-        'three': [{'report_date': '06/04/2019'}]
-    }, {
-        'one': [{'report_date': '06/10/2019'}, {'report_date': '06/11/2019'}, {'report_date': '06/12/2019'}],
-        'four': [{'report_date': '06/10/2019'}]
-    }]
-
-    assert merge_reports(records, date(2019, 5, 31), date(2019, 6, 10)) == {
-        'one': [{'report_date': '05/31/2019'}, {'report_date': '06/05/2019'}, {'report_date': '06/10/2019'}],
-        'two': [{'report_date': '06/03/2019'}],
-        'three': [{'report_date': '06/04/2019'}],
-        'four': [{'report_date': '06/10/2019'}]
-    }
 
 
 @fixture
@@ -85,26 +64,6 @@ async def test_query_sections(repository: Repository):
     assert volume[0]['report_date'] == '06/03/2019'
     assert volume[1]['report_date'] == '06/04/2019'
     assert volume[-1]['report_date'] == '06/10/2019'
-
-
-@mark.asyncio
-async def test_query_report(repository: Repository):
-    async with server():
-        report = await repository.query(date(2019, 6, 3), date(2019, 6, 8))
-
-    assert len(report) == 14
-
-    cutout = report[CutoutReport.Section.CUTOUT]
-    assert len(cutout) == 5
-    assert cutout[0]['report_date'] == '06/03/2019'
-    assert cutout[1]['report_date'] == '06/04/2019'
-    assert cutout[-1]['report_date'] == '06/07/2019'
-
-    volume = report[CutoutReport.Section.VOLUME]
-    assert len(volume) == 5
-    assert volume[0]['report_date'] == '06/03/2019'
-    assert volume[1]['report_date'] == '06/04/2019'
-    assert volume[-1]['report_date'] == '06/07/2019'
 
 
 @mark.asyncio
